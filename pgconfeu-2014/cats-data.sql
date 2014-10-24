662ad39008b6e5f86cd97d7ffe94acf7dae585da
David Fetter <david@fetter.org>
2014-10-24 10:37:16 -0700

    Update cats-data.sql

--- pgconfeu-2014/cats-data.sql
+++ pgconfeu-2014/cats-data.sql
@@ -1,11 +1,8 @@
--- psql -U postgres -d kittycats < cats-data.sql
+-- psql -U postgres -1f cats-data.sql kittycats
 
 INSERT INTO cats (name, picture, breed, origin, birthday)
-  VALUES ('Kitty', 'fritz.jpg', 'Burmese', 'TH', '2010-12-06');
-INSERT INTO cats (name, picture, breed, origin)
-  VALUES ('Missy', 'fritz-comfy.jpg', 'Chartreux', 'FR');
-INSERT INTO cats (name, picture, breed, origin, birthday)
-  VALUES ('Sassy', 'fritz-desk.jpg', 'Mau', 'EG', '2008-08-04');
-INSERT INTO cats (name, picture, breed, origin, birthday) VALUES
-  ('Mitzi', 'fritz-sit.jpg', 'Norwegian Forest Cat', 'NO', '2011-03-03');
+  VALUES ('Kitty', 'fritz.jpg', 'Burmese', 'TH', '2010-12-06'),
+         ('Missy', 'fritz-comfy.jpg', 'Chartreux', 'FR'),
+         ('Sassy', 'fritz-desk.jpg', 'Mau', 'EG', '2008-08-04'),
+         ('Mitzi', 'fritz-sit.jpg', 'Norwegian Forest Cat', 'NO', '2011-03-03');
 
